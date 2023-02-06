ddd8f973e1a99e6759ad17b91d31535a172826d6
Troy Jennings <troy.c.jennings@gmail.com>
2023-02-05 22:31:13 -0700

    Feature: Implement split data flow
    - Update all dependencies for Prefect flows
    - Define base review data object as custom pydantic class
    - Implement split_data flow

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # E-commerce MLOps
-A repository for demonstrating MLOps architecture for Amazon e-commerce reviews.
+A repository for demonstrating DataOps and MLOps architectural concepts for a toy Amazon e-commerce problem.
+
+
+# To Dos
+- ~~Obtain Amazon review data~~
+- ~~Add data pipeline dependencies~~
+- Build a data preprocessing pipeline in Prefect
+    - Ingest from URL?
+    - Split dataset
+    - Feature-engineering
+        - Build a spaCy pipeline to generate lemmatized tokens from the review text
+        - Build a spaCy pipeline to create embeddings from the review text
+- Store data on Azure blob storage ("raw" storage)
+- Setup Feast to ingest data into feature store (from local)
+
+
+# Dependencies
+- Terraform for all Azure resource creation
+- Feast for feature store
+- Prefect for orchestration
+
 
 # Citations
 [1] "_Justifying recommendations using distantly-labeled reviews and fined-grained aspects_", Jianmo Ni, Jiacheng Li, Julian McAuley, Empirical Methods in Natural Language Processing (EMNLP), 2019
 
 https://nijianmo.github.io/amazon/index.html
-
-# To Dos
-- ~~Obtain Amazon review data~~
-- ~~Add Feast to dependencies~~
-- Build a spaCy pipeline to generate lemmatized tokens from the review text
-- Build a spaCy pipeline to create embeddings from the review text
-
-
-- Store data on Azure blob storage ("raw" storage)
-- Outline ingestion pipeline
-    - When/how to ingest incrementally? CosmosDB for tracking dates ingested?
-- Setup Feast to ingest data into feature store
-
-# Dependencies
-- Terraform for all Azure resource creation
-- Feast for feature store
---
+++ flows/split_data.py
@@ -0,0 +1,90 @@
+import gzip
+import json
+import os
+
+from collections import defaultdict
+from typing import Dict, List, TypedDict
+
+from dotenv import load_dotenv
+from prefect import flow, task
+
+from models import ReviewData
+
+load_dotenv(override=True)
+
+
+# define flow tasks
+@task(
+	name="Aggregate data by timestamp",
+	description="Aggregates reviews by their unix timestamp."
+)
+def aggregate_data_by_timestamp(
+	source_input_path: str
+) -> Dict[str, List[ReviewData]]:
+	"""
+	Parameters
+	----------
+	source_input_path : str
+		Path to the input source file
+
+	Returns
+	-------
+	output : Dict[str, List[ReviewData]]]
+		Dictionary with timestamp as keys and list of review data as values
+	"""
+	output = defaultdict(list)
+	with gzip.open(source_input_path, "r") as f_in:
+		for line in f_in.readlines():
+			element = json.loads(line)
+			output[element["unixReviewTime"]].append(element)
+	return output
+
+
+@task(
+	name="Write data by timestamp",
+	description="Writes individual json output for each timestamp."
+)
+def write_data_by_timestamp(
+	data: Dict[str, List[ReviewData]],
+	processed_output_path: str
+) -> None:
+	"""
+	Parameters
+	----------
+	data : Dict[str, List[ReviewData]]
+		Timestamp-aggregated json from prior task
+	processed_output_path : str
+		Path to write the output data
+	"""
+	for key, value in data.items():
+		file_path = os.path.join(processed_output_path, f"{key}.json")
+		with open(file_path, "w") as f_out:
+			f_out.write(json.dumps(value))
+
+
+# define flow and subflows
+@flow(
+	name="Split data",
+	description="Flow to handle data pre-processing"
+)
+def split_data(
+	source_input_path: str,
+	processed_output_path: str
+):
+	aggregated_data = aggregate_data_by_timestamp(
+		source_input_path=source_input_path
+	)
+	write_data_by_timestamp(
+		data=aggregated_data,
+		processed_output_path=processed_output_path
+	)
+
+
+if __name__ == "__main__":
+	source_input_path = os.getenv("SOURCE_INPUT_PATH")
+	processed_output_path = os.getenv("PROCESSED_OUTPUT_PATH")
+
+	split_data(
+		source_input_path=source_input_path,
+		processed_output_path=processed_output_path
+	)
