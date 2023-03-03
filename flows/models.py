47227cf6241ea6d6ca6944e1efbd75a8de5e98fc
Troy Jennings <troy.c.jennings@gmail.com>
2023-03-02 21:17:16 -0700

    Implement basic data pipeline with Prefect (#2)
    - Update all dependencies for Prefect flows
    - Define base review data object as custom pydantic classes
    - Implement split_data flow for preprocessing
    - Implement transform_data flow for featurization

--- flows/models.py
+++ flows/models.py
@@ -4,9 +4,15 @@ class ReviewData(BaseModel):
 	overall: float
 	verified: bool
 	reviewTime: str
-	reviewerId: str
+	reviewerID: str
 	asin: str
 	reviewerName: str
 	reviewText: str
+	tokenizedReviewText: list[str]
 	summary: str
+	tokenizedSummary: list[str]
 	unixReviewTime: int
+
+class FeaturizedReviewData(ReviewData):
+	featurizedReview: list[str]
+	featurizedSummary: list[str]
